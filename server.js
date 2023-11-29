a6f34b99e76a0749c894a34e4cb39fcfcfaa0031
nxc03190 <134020121+nxc03190@users.noreply.github.com>
2023-11-29 16:59:06 -0600

    Updated port logic

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http')
 const app = require('./app')
-const port = 80
+//const port = 3100
+const port = parseInt(process.env.PORT ?? '3100');
+console.log(port)
 const server = http.createServer(app)
 server.listen(port)
