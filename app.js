606df01e3848ae9659ea65832435c37358d174ba
nxc03190 <134020121+nxc03190@users.noreply.github.com>
2023-11-29 15:55:24 -0600

    Updated header

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 app.use(express.json());
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*')
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-Width, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization')
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, GET, DELETE')
         return res.status(200).json({})
